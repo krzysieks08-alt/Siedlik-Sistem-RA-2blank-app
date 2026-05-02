2ea835c8038b9239bbfdd6b79e823eb4ba582f15
krzysieks08-alt <krzysieks08@gazeta.pl>
2026-05-02 03:35:25 +0200

    Delete index.py

--- index.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-
-def main(page: ft.Page):
-    # ... tutaj cały Twój kod który wklejaliśmy wcześniej ...
-    page.add(ft.Text("SIEDLIK SYSTEM DZIAŁA!"))
-
-# Zmień tę końcówkę na taką:
-app = ft.app(target=main, view=ft.AppView.WEB_BROWSER)
